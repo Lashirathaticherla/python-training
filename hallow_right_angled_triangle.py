d82ac1508649c32853195f951d4045eae24b9a02
Lashirathaticherla <lashira1424@gmail.com>
2025-12-27 15:29:28 +0530

    Add hollow right-angled triangle pattern
    Implement hollow right-angled triangle pattern

---
+++ hallow_right_angled_triangle.py
@@ -0,0 +1,9 @@
+# hallow right angled tringle pattern
+n =int(input())
+for i in range(n):
+    for j in range(i+1):
+      if i==n-1 or j==0 or i==j:
+        print("*",end=" ")
+      else:
+        print(" ",end=" ")
+    print()    
