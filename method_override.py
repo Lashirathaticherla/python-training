e4a8d6d74cd0084a8bb61bc6d5bd1e2e58b664c1
Lashirah Thaticherla <lashirahthaticherla@gmail.com>
2025-12-30 14:01:13 +0530

    Add files via upload

---
+++ method_override.py
@@ -0,0 +1,8 @@
+class MethodOverRide1:
+    def display(self):
+        print("method involved from base class")
+class MethodOverRide2:
+    def display(self):
+        print("method involved from derived class")
+ob=MethodOverRide2()
+ob.display()
