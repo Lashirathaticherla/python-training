383883f56c77cc1e08336d1e53641d407a5344a1
Lashirah Thaticherla <lashirahthaticherla@gmail.com>
2025-12-30 15:06:24 +0530

    Add files via upload

---
+++ Bubble_sort.py
@@ -0,0 +1,9 @@
+def Bubble_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+arr= list(map(int,input().split(",")))    
+Bubble_sort(arr)
+print("sorted array:",arr)
